26bc31fdc97d4f55bee0ddf425dff9ed482cc280
Magnus Gideryd <mgideryd@gmail.com>
2014-06-08 14:26:17 +0200

    Woek

--- Apps/NinjaDee.App/main.cpp
+++ Apps/NinjaDee.App/main.cpp
@@ -4,7 +4,4 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int nCmdShow)
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
-	int* sd = new int;
-	delete sd;
 }
